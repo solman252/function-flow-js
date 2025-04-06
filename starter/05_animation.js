576ef6632ef40e6e6048f67d3075c3aed32c6cfd
Solman252 <undertalelover909@gmail.com>
2025-04-07 10:58:37 +1200

    Rcoket

--- starter/05_animation.js
+++ starter/05_animation.js
@@ -1,8 +1,18 @@
 // TODO: Write a function 'move' that simulates a rocket moving back and forth in the console
 // TODO: Use 'console.clear()', 'console.log()', and basic logic with position
 
+const rocketString = 'X=>'
+let rocketMargin = ''
+
 function move() {
   // Your code here
+  console.clear()
+  console.log(rocketMargin+rocketString)
+  rocketMargin += ' '
+}
+
+for (let i = 0; i < 20; i++) {
+  setTimeout(move,i*500)
 }
 
 module.exports = { move };
