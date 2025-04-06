c5c710feac644a637b36833d62c1ce5abaf6aa30
Solman252 <undertalelover909@gmail.com>
2025-04-07 11:02:08 +1200

    Rocket done

--- starter/05_animation.js
+++ starter/05_animation.js
@@ -2,17 +2,19 @@
 // TODO: Use 'console.clear()', 'console.log()', and basic logic with position
 
 const rocketString = 'X=>'
-let rocketMargin = ''
+let rocketMargin = 0
 
-function move() {
+function move(speed) {
   // Your code here
   console.clear()
-  console.log(rocketMargin+rocketString)
-  rocketMargin += ' '
+  console.log(' '.repeat(rocketMargin)+rocketString)
+  rocketMargin += speed
 }
 
 for (let i = 0; i < 20; i++) {
-  setTimeout(move,i*500)
+  setTimeout(() => {
+    move(1)
+  }, 500*i);
 }
 
 module.exports = { move };
