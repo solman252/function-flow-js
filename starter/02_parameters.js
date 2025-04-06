8d1a38338d9089d3096b01fcb50cf0c51d8d4c38
Solman252 <undertalelover909@gmail.com>
2025-04-07 10:23:09 +1200

    02 Done

--- starter/02_parameters.js
+++ starter/02_parameters.js
@@ -1,8 +1,21 @@
 // TODO: Write a function 'totalScores' that takes an array of numbers
 // TODO: Return the sum of all numbers in the array
 
+let ExampleScores = [0,12,8,5]
+
 function totalScores(scores) {
   // Your code here
+  // Iterare through the array of scores
+  let total = 0
+  for (let index = 0; index < scores.length; index++) {
+    const element = scores[index];
+      // Add to the total
+      total += element
+  }
+  // Print the total
+  console.log(total)
 }
+
+totalScores(ExampleScores)
 
 module.exports = { totalScores };
