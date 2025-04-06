9fb0843c1634af85faec877dded8bac719480dc2
Solman252 <undertalelover909@gmail.com>
2025-04-07 10:12:14 +1200

    Student Example

--- starter/01_basics.js
+++ starter/01_basics.js
@@ -1,8 +1,15 @@
 // TODO: Create a student object with 'name' and 'age' properties
 // TODO: Write a function 'printStudentName' that logs the student's name
 
-function printStudentName() {
+let ExampleStudent = {name:"Bob",age:32}
+let ExampleStudentB = {name:"Who",age:32}
+
+function printStudentName(student) {
   // Your code here
+  console.log(student.name)
 }
+
+printStudentName(ExampleStudent)
+printStudentName(ExampleStudentB)
 
 module.exports = { printStudentName };
