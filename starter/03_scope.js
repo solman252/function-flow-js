8a7b49e695e46bd8dcee273444301a9274920e8c
PCykana <peter-cykana@live.nmit.ac.nz>
2025-04-07 10:33:42 +1200

    03_scope solution

--- starter/03_scope.js
+++ starter/03_scope.js
@@ -7,10 +7,15 @@ let role = "guest";
 
 function setRole() {
   // Your code here
+  let role = "admin" // declares local variable inside function called role and assigns value admin
+  console.log(role) // prints local variable
 }
 
 function showGlobalRole() {
   // Your code here
+  console.log(role)// prints global variable
 }
+setRole() // calls function setRoll with the local role variable
+showGlobalRole() // calls fucntion showGlobalRole with the global variable
 
 module.exports = { setRole, showGlobalRole, role };
