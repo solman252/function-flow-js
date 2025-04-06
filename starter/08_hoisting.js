84db0b8ce87c3f306579e45a4b377eadc648fb0b
PCykana <peter-cykana@live.nmit.ac.nz>
2025-04-07 11:25:12 +1200

    hoisting solution

--- starter/08_hoisting.js
+++ starter/08_hoisting.js
@@ -1,12 +1,18 @@
 // TODO: Write a function 'greet' and call it before its definition
 // TODO: Write a function 'showUserData' where you use a variable before declaring it with var
 
+greet()
+
 function greet() {
   // Your code here
+  console.log('Hello user')
 }
 
 function showUserData() {
   // Your code here
+  console.log(userData)
 }
-
+showUserData()
+var userData = ['Bob', 32]
+showUserData()
 module.exports = { greet, showUserData };
